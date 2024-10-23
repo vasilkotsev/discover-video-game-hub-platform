eb608582ac8cee028d6f01298094f5f21558f173
vasilkotsev <vasil.kotsev@gmail.com>
2024-10-23 14:04:52 +0300

    Implement fetching the genres with useGenres custom hook

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,20 @@
+import { Text } from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+
+const GenreList = () => {
+  const { genres, error } = useGenres();
+
+  console.log("Render List component");
+  return (
+    <>
+      {error && <Text>{error}</Text>}
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default GenreList;
