5896876002d1ffa2ceafcde36e711652fd8291c2
vasilkotsev <vasil.kotsev@gmail.com>
2024-10-23 14:09:45 +0300

    Change GameGrid component export

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
-export const GameGrid = () => {
+const GameGrid = () => {
   const { games, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
@@ -31,3 +31,5 @@ export const GameGrid = () => {
     </>
   );
 };
+
+export default GameGrid;
