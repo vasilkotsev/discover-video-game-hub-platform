fcfe49e0dbefad6bd3eab3a653ab69f9d5210622
vasilkotsev <vasil.kotsev@gmail.com>
2024-10-24 01:01:35 +0300

    Implement a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
-  const { genres, error } = useGenres();
+  const { data: genres, error } = useGenres();
 
   console.log("Render List component");
   return (
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "axios";
-import apiClient from "../services/api-client";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
@@ -16,36 +14,6 @@ export interface Game {
   metacritic: number;
 }
 
-interface FetchGamesResponseObj {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponseObj>("/games", { signal: controller.signal })
-      .then((res) => {
-        setGames(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, isLoading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
