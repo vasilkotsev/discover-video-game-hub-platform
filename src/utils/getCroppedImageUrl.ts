765a5f477ef1e792863ebe3b064bccf770587f21
vasilkotsev <vasil.kotsev@gmail.com>
2024-10-29 20:02:42 +0200

    Fix:Handle games without image

--- src/utils/getCroppedImageUrl.ts
+++ src/utils/getCroppedImageUrl.ts
@@ -1,4 +1,6 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
+
 const getCroppedImageUrl = (url: string) =>
-  url?.replace("/media/", "/media/crop/600/400/") || "";
+  url?.replace("/media/", "/media/crop/600/400/") || noImage;
 
 export default getCroppedImageUrl;
