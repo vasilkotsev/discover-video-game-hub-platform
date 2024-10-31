550a463c72e495c89741ecdb0219e1511c9d97cf
vasilkotsev <vasil.kotsev@gmail.com>
2024-10-31 22:11:25 +0200

    Customize the theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#eceff8",
+      100: "#c9cfe6",
+      200: "#a4aed5",
+      300: "#808ec7",
+      400: "#5c6eb9",
+      500: "#4455a0",
+      600: "#35427b",
+      700: "#262f58",
+      800: "#161c34",
+      900: "#070913",
+    },
+  },
+});
 
 export default theme;
